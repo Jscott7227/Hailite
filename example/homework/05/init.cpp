222afff9bc5d524870e69346446dc911dab8a252
jlscott44 <jlscott44@login.localdomain>
2024-03-04 15:27:05 -0600

    tested

--- example/homework/05/init.cpp
+++ example/homework/05/init.cpp
@@ -1,12 +1,14 @@
 #include <Kokkos_Core.hpp>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 // Create a program that compares a parallel for loop and a standard for loop for summing rows of a View with Kokkos Timer.
 
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
+
   // Make View and create values
   int n = 16;
   Kokkos::View<int*> A("A", n);
@@ -15,28 +17,29 @@ int main(int argc, char* argv[]) {
 
 		A(i) = (rand() % (1000 - 0 + 1))*i;
     });
-  
+  Kokkos::View<int*> parallel_sum("Sum",1)
   //Create Timer time starts here
   Kokkos::Timer timer;
 
-  int parallel_sum = 0;
 
   // Parallel Sum  
   Kokkos::parallel_for("Loop1", A.extent(0), KOKKOS_LAMBDA (const int i) {
 
-		parallel_sum += A(i);
-    });
+		parallel_sum[0] += A(i);
+    }) ;
   
-  double parallel_time = timer.seconds;
+  double parallel_time = timer.seconds();
   //Reset Timer 
   timer.reset();
+
+  int serial_sum = 0;
 
   //Serial Sum
   for (int i = 0; i < n; i++) {
     serial_sum += A(i);
   }
 
-  double serial_time = timer.seconds;
+  double serial_time = timer.seconds();
   timer.reset();
   // Output times
   std::cout << "Parallel time: " << parallel_time << std::endl;
